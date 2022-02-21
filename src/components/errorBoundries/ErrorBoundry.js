77234f6c3fba4f4376512682a9077bc02be2e4d0
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-02-21 16:46:49 +0500

    error boundary successfully added

--- src/components/errorBoundries/ErrorBoundry.js
+++ src/components/errorBoundries/ErrorBoundry.js
@@ -13,7 +13,15 @@ class ErrorBoundry extends Component {
 
   render() {
     if (this.state.hasError) {
-      return <p>Error Boundary: Something went wrong!</p>;
+      return (
+        <div
+          className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <span className="font-medium">Error Boundary:</span> Something went
+          wrong in your application!
+        </div>
+      );
     }
     return this.props.children;
   }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,27 +3,12 @@ import MainNavigation from "./MainNavigation";
 import Footer from "./Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import { useState } from "react";
 
 const Layout = (props) => {
-  const [state, setState] = useState(true);
-
-  if (!state) {
-    throw new Error("error in layout");
-  }
-
   return (
     <div className="layout">
       <MainNavigation />
-
       <main className="main mt-67">
-        <button
-          onClick={() => {
-            setState((state) => !state);
-          }}
-        >
-          Test error boundry
-        </button>
         <Outlet />
       </main>
       <Footer />
